6c85332bb6a2bcf3825d79fc7419c5c18482252f
Edgar Jesus <mendesbr92@gmail.com>
2023-09-03 19:25:52 -0300

    criado tabela alunos

--- OneDrive/Desktop/mysql/mysql_aula_1.sql
+++ OneDrive/Desktop/mysql/mysql_aula_1.sql
@@ -1,13 +1,24 @@
-create database cadastro
-default character set utf8 
-default collate utf8_general_ci;
+create database cadastro_academia default collate utf8_general_ci;
 
-create table pessoas (
-	id int not null auto_increment,
-	nome varchar (30) not null,
-    nacimento date,
-    sexo enum ('Masculino', 'Feminino'),
-    peso decimal (5 , 2),
-    nacionalidade varchar (20) default 'Brasil',
-    primary key (id)
-) default charset = utf8;
+select * from alunos;
+
+	create table alunos (
+		id  int not null auto_increment primary key,
+        nome varchar (30) not null,
+        nascimento date,
+        sexo enum ('M', 'F'),
+        peso decimal (5 , 2),
+        altura decimal (3 , 2),
+        nacionalidade varchar (15) default ('Brasil')
+    ) default charset = utf8;
+
+	insert into alunos (nome, nascimento, sexo, peso, altura, nacionalidade) values ('Godofredo','1984-01-02','M', '78.50', '1.83','Russia');
+    insert into alunos (nome, nascimento, sexo, peso, altura, nacionalidade) values ('Maria', '1992-12-30', 'F', '55.2', '1.65', 'Portugal');
+	insert into alunos (nome, nascimento, sexo, peso, altura, nacionalidade) values ('Creuza', '1920-12-30', 'F', '50.2', '1.72', default);
+	
+    insert into alunos values (DEFAULT, 'Adalgiza', '1930-11-2', 'F', '63.50' , '1.78' , 'Irlanda' );
+
+	select * from alunos;
+
+
+
