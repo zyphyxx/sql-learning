d3279fdc0d8863e6ee0c5520b2366d94a990ff8e
Edgar Jesus <mendesbr92@gmail.com>
2023-09-06 20:17:30 -0300

    aula sobre insert into Impacta

---
+++ OneDrive/Desktop/mysql/mysql_aula_1_Impacta.sql
@@ -0,0 +1,52 @@
+create database if not exists empresa;
+
+use empresa;
+
+create table if not exists clientes(
+	idCliente int not null primary key auto_increment,
+    nomeCliente varchar (30) not null,
+    sexoCliente enum ('M', 'F'),
+    ufNascimento char (2),
+    dataNascimento date
+	);
+    
+    desc clientes;
+    
+    -- DATA MANIPULATION LANGUAGE
+    -- INSERT UPADATE DELETE
+    
+    -- INSERT DECLARATORIO - DECLARO OS CAMPOS QUE RECEBER
+    
+    insert into clientes (nomeCliente,sexoCliente,ufNascimento,dataNascimento)
+		values
+        ('Alem Mar Paranhos','M','SP','1995-05-30');
+	
+    insert into clientes (nomeCliente,sexoCliente)
+		values
+        ('Suzana Maria','M');
+	
+   -- INSERT POSICIONAL
+   
+	insert into clientes values
+		(null,'Jussara Paula','F','SC', '2000-05-21');
+	
+    insert into clientes values
+		(null,'João Carlos','M' , null, null);
+        
+	-- DECLARATORIO
+    
+		insert into clientes (nomeCliente,SexoCliente) values ('Suzi Rego','F'), ('Ana Melo', 'F'), ('Jorge Freitas', 'M');
+		
+        
+        select * from clientes;
+        
+        
+        
+    
+    
+    
+    
+    
+
+    
+    
